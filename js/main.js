484b8a370733cb52e380fbda17362f7e947249bc
mru2-loves-computers <waldi240574@gmail.com>
2025-02-01 19:41:30 +0000

    menu/pages structure

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const app = {
     async init() {
         console.log("All Amiga READY");
+        this.jq = $;
+        this.trigger = this.jq('.trigger');
+        this.trigger.on('click',(e)=>{ this.initTrigger(e); });
+    },
+    async initTrigger(e) {
+        e.preventDefault();
+        let target = this.jq(e.currentTarget);     
+        if(target.hasClass('menu')) {
+            this.initMenu(target);
+        }        
+    },
+    async initMenu(target) {
+        let targetLink = target.attr('href');            
+        this.jq("#content").load('../pages/'+targetLink+'.html');
     }
 }
 
